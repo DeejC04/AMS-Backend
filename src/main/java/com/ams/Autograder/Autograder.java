05bbbf2ff741fe2635642a6ca3848a776ae0354c
Archit Singh <91148814+architsingh0@users.noreply.github.com>
2023-06-25 17:28:50 -0700

    Many Changes
    -Added .env changes to prevent any leaks of API key
    -Fixed an issue with assignment ids
    -Fixed an issue with assignments not being graded
    -Added support for multiple courses

--- src/main/java/com/ams/Autograder/Autograder.java
+++ src/main/java/com/ams/Autograder/Autograder.java
@@ -1,58 +1,77 @@
 package com.ams.Autograder;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import edu.ksu.canvas.CanvasApiFactory;
+import edu.ksu.canvas.interfaces.AssignmentReader;
 import edu.ksu.canvas.interfaces.AssignmentWriter;
+import edu.ksu.canvas.interfaces.CourseReader;
 import edu.ksu.canvas.model.assignment.Assignment;
 import edu.ksu.canvas.interfaces.EnrollmentReader;
 import edu.ksu.canvas.interfaces.SubmissionWriter;
+import edu.ksu.canvas.model.Course;
 import edu.ksu.canvas.model.Enrollment;
 import edu.ksu.canvas.oauth.OauthToken;
 import edu.ksu.canvas.requestOptions.GetEnrollmentOptions;
+import edu.ksu.canvas.requestOptions.ListActiveCoursesInAccountOptions;
+import edu.ksu.canvas.requestOptions.ListCourseAssignmentsOptions;
 import edu.ksu.canvas.requestOptions.MultipleSubmissionsOptions;
+import io.github.cdimascio.dotenv.Dotenv;
 import edu.ksu.canvas.oauth.NonRefreshableOauthToken;
+
+
 
 public class Autograder {
 
+    private static final CanvasApiFactory apiFactory;
+    private static final OauthToken oauthToken;
+    static {
+        Dotenv env = Dotenv.load();
+        apiFactory = new CanvasApiFactory(env.get("CANVAS_URL"));
+        oauthToken = new NonRefreshableOauthToken(env.get("APIKEY"));
+    }
+
     public static void main(String[] args) throws IOException{
-        //creating a connection to a canvas instance
-        String canvasBaseUrl = "http://104.237.159.242:3000/";
-        OauthToken oauthToken = new NonRefreshableOauthToken("");
-        CanvasApiFactory apiFactory = new CanvasApiFactory(canvasBaseUrl);
+        
+        CourseReader courseReader = apiFactory.getReader(CourseReader.class, oauthToken);
+        for(Course course : courseReader.listActiveCoursesInAccount(new ListActiveCoursesInAccountOptions("1"))){
 
-        //creating an assignment for every student in the course
-        Assignment attendanceAssignment = new Assignment();
-        attendanceAssignment.setName("Attendance");
-        attendanceAssignment.setGradingType("points");
-        attendanceAssignment.setPointsPossible(5.0);
-        attendanceAssignment.setPublished(true);
-        attendanceAssignment.setUnpublishable(false);
-        attendanceAssignment.setId(22L);
-    
-        List<String> list = new ArrayList<String>();
-        list.add("on_paper");
-        attendanceAssignment.setSubmissionTypes(list);
+            //creating an assignment for every student in the course
+            Assignment attendanceAssignment = new Assignment();
+            attendanceAssignment.setName("Attendance");
+            attendanceAssignment.setGradingType("points");
+            attendanceAssignment.setPointsPossible(5.0);
+            attendanceAssignment.setPublished(true);
 
-        AssignmentWriter assnWriter = apiFactory.getWriter(AssignmentWriter.class, oauthToken);
-        assnWriter.createAssignment("1", attendanceAssignment);
+            AssignmentWriter assnWriter = apiFactory.getWriter(AssignmentWriter.class, oauthToken);
+            assnWriter.createAssignment(course.getId().toString(), attendanceAssignment);
 
+            AssignmentReader assnReader = apiFactory.getReader(AssignmentReader.class, oauthToken);
+            for(Assignment attendance : assnReader.listCourseAssignments(new ListCourseAssignmentsOptions(course.getId().toString()))){
+                attendanceAssignment.setId(attendance.getId());
+            }
+
+            System.out.println(attendanceAssignment.getId());
+
+            GradeAssignment(attendanceAssignment, course);
+
+            // listing every user inside a course 
+            EnrollmentReader enrlReader = apiFactory.getReader(EnrollmentReader.class, oauthToken);
+            for(Enrollment enroll : enrlReader.getCourseEnrollments(new GetEnrollmentOptions(course.getId().toString()))){
+                System.out.println(enroll.getUser().getName());
+            }
+        }
+    }
+
+    public static void GradeAssignment(Assignment attendanceAssignment, Course course) throws IOException{
         SubmissionWriter submissionWriter = apiFactory.getWriter(SubmissionWriter.class, oauthToken);
         Map<String, MultipleSubmissionsOptions.StudentSubmissionOption> mapOfOptions = new HashMap<>();
-        
-        MultipleSubmissionsOptions submissionsOptions = new MultipleSubmissionsOptions("1", 22L, null);
-        mapOfOptions.put("7", submissionsOptions.createStudentSubmissionOption("null", "5.0", false, false, "null", "null"));
+
+        MultipleSubmissionsOptions submissionsOptions = new MultipleSubmissionsOptions(course.getId().toString(), attendanceAssignment.getId(), mapOfOptions);
+        mapOfOptions.put("52", submissionsOptions.createStudentSubmissionOption("null", "5.0", false, false, "null", "null"));
         submissionsOptions.setStudentSubmissionOptionMap(mapOfOptions);
         submissionWriter.gradeMultipleSubmissionsByCourse(submissionsOptions);
-
-        //listing every user inside a course 
-        EnrollmentReader enrlReader = apiFactory.getReader(EnrollmentReader.class, oauthToken);
-        for(Enrollment enroll : enrlReader.getCourseEnrollments(new GetEnrollmentOptions("1"))){
-            System.out.println(enroll.getUser().getName());
-        }
     }
 }
